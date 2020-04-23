57e9e7da493afe336ac335f0bd84476623c89500
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-04-23 14:38:39 -0400

    change format to png

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,7 @@ exports.run = (bot, msg, args) => {
 	msg.channel.createMessage({embed: {
 		color: 0x38B1D0,
 		thumbnail: {
-			url: bot.user.avatarURL.replace("128", "512")
+			url: bot.user.avatarURL.replace("128", "512").replace("jpg", "png")
 		},
 		description: "[http.cat](https://http.cat) is a site to look at HTTP codes with cat images, and the bot provides you with exactly that also.\nIf you want a command list, run `http.cat commands` and if you want to see a code run `http.cat <code>`."
 	}})
