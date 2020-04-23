95db77668c332f435bf1c76ce8ae55703f783ca3
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-04-23 12:26:40 -0400

    use bott instead of client and stuff

--- index.js
+++ index.js
@@ -2,4 +2,29 @@ const Eris = require("eris")
 require("dotenv").config()
 const bot = new Eris(process.env.TOKEN)
 
+try {
+	let files = require("fs").readdirSync("./events").filter(f => f.split(".").pop() === "js")
+	if(files.length === 0) {
+		console.log("No events to load!")
+	} else {
+		let e = 0;
+		for(let i = 0; i < files.length; i++) {
+			const _event = files[i].slice(0, -3)
+			try {
+				const event = require(`./events/${files[i]}`)
+				bot.on(_event, event.bind(null, bot))
+				console.log(`Successfully loaded event ${_event}.`)
+				e++
+			} catch(err) {
+				const trace = err.stack.toString().split("\n").slice(0, 3).join("\n")
+				console.log(`An error occurred while trying to load ${_event}\n${trace}`)
+				console.log(`Could not load the event ${_event}.`)
+			}
+		}
+		console.log(`Loaded ${e} events.`)
+	}
+} catch(err) {
+	console.error(err)
+}
+
 bot.connect()
