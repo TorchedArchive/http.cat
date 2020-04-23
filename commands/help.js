d8a1d1684ad3f60810a9e39fd02f708597c9c5e5
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-04-23 14:36:26 -0400

    help command for info about the bot

---
+++ commands/help.js
@@ -0,0 +1,13 @@
+exports.run = (bot, msg, args) => {
+	msg.channel.createMessage({embed: {
+		color: 0x38B1D0,
+		thumbnail: {
+			url: bot.user.avatarURL.replace("128", "512")
+		},
+		description: "[http.cat](https://http.cat) is a site to look at HTTP codes with cat images, and the bot provides you with exactly that also.\nIf you want a command list, run `http.cat commands` and if you want to see a code run `http.cat <code>`."
+	}})
+}
+
+exports.info = {
+	name: "help"
+}
