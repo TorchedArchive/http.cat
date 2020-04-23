09d9dafa9e513b778b056d12084b0e77aec3fc7b
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-04-23 14:54:29 -0400

    add map and filter function to Map class

--- index.js
+++ index.js
@@ -55,3 +55,21 @@ try {
 }
 
 bot.connect()
+
+Map.prototype.filter = function(fn, thisArg) {
+	if (thisArg) fn = fn.bind(thisArg);
+    const results = new Map();
+    for (const [key, val] of this) {
+    	if (fn(val, key, this)) results.set(key, val);        
+    }
+    return results;
+      
+}
+
+Map.prototype.map = function(fn, thisArg) {
+    if (thisArg) fn = fn.bind(thisArg);
+    const arr = new Array(this.size);
+    let i = 0;
+    for (const [key, val] of this) arr[i++] = fn(val, key, this);
+    return arr;
+}
